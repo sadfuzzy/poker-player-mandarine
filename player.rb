e231f935a5c20bcc88dfe31ed45ce41ce8a0a9c1
Oleg V Artamonov <oleg@artamonov.ru>
2015-09-27 11:00:25 +0300

    + some ++

--- player.rb
+++ player.rb
@@ -4,16 +4,25 @@ class Player
   VERSION = "Default Ruby folding player"
 
   def bet_request(game_state)
-    puts "# ---"
-    puts game_state["players"].inspect
-    puts game_state["community_cards"].inspect
-    puts "# ==="
-    me = game_state["players"].select{|p| p["name"] == "Mandarine"}
+    # hard c
+    @game_state = game_state
+      
+    #puts game_state["players"].inspect
+    #puts game_state["community_cards"].inspect
+    
     puts me.inspect
-    rand(333)
+    rand(506) + 100
   end
 
   def showdown(game_state)
 
+  end
+
+  def opponents
+    # 
+  end
+
+  def me
+    @game_state["players"].select{|p| p["name"] == "Mandarine"}
   end
 end
