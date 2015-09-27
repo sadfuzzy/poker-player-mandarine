7dd5b7223f6fa2a9761778bbeef5db747b03c338
Oleg V Artamonov <oleg@artamonov.ru>
2015-09-27 14:24:42 +0300

    +-

--- player.rb
+++ player.rb
@@ -1,19 +1,17 @@
-#require './game_info'
+require './game_info'
 
 class Player
-  #include GameInfo
+  include GameInfo
   VERSION = "All in strategy"
   PLAYER_NAME = 'Mandarine'
 
   def bet_request(game_state)
-    #puts "START bet_request::"
-    #@game_state = game_state
-    #bet = select_bet
+    puts "START bet_request::"
+    @game_state = game_state
+    bet_size = select_bet
 
-    #puts "bet #{bet} # with: #{my_cards} table: #{table_cards}"
-    #bet
-    puts "bet 1000"
-    1000
+    puts "bet #{bet_size} # with: #{my_cards} table: #{table_cards}"
+    bet_size
   end
 
   def showdown(game_state)
