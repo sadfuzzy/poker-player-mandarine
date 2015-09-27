0c1020de9039a81c9744ddd5cb87110b7e429284
Denis Savitskiy <sadfuzzy@yandex.ru>
2015-09-27 13:50:10 +0300

    Rebase.

--- player.rb
+++ player.rb
@@ -27,39 +27,10 @@ class Player
     puts "table_cards : #{table_cards}"
     puts "all_cards: #{all_cards}"
     #puts my_cards.inspect
-    select_bet
-    #bet
-  #rescue
-    #1000
-  end
 
+    select_bet
+  end
 
   def showdown(game_state)
   end
-
-  def community_cards(data)
-    data['community_cards']
-  end
-
-  def player_data(data)
-    data['players'].select{|e| e['name'] == PLAYER_NAME}
-  end
-
-  def my_cards_second(data)
-    player_data(data)['hole_cards']
-  end
-
-  def detect_pair(cards)
-    ranks = cards.map{ |card| card['rank'] }
-    ranks.detect{ |card| ranks.count(card) >= 2 }
-  end
-
-  def has_pair?(game_state)
-    if game_state
-      all_cards = community_cards(game_state) + my_cards(game_state)
-
-      detect_pair(all_cards) if all_cards.size > 0
-    end
-  end
-
 end
