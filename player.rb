c6cc3d0619d4651dabd2b5e3427b7ceb74f7765d
Oleg V Artamonov <oleg@artamonov.ru>
2015-09-27 14:22:48 +0300

    +-

--- player.rb
+++ player.rb
@@ -1,17 +1,18 @@
-require './game_info'
+#require './game_info'
 
 class Player
-  include GameInfo
-  VERSION = "Default Ruby folding player"
+  #include GameInfo
+  VERSION = "All in strategy"
   PLAYER_NAME = 'Mandarine'
 
   def bet_request(game_state)
-    puts "START bet_request::"
-    @game_state = game_state
-    bet = select_bet
+    #puts "START bet_request::"
+    #@game_state = game_state
+    #bet = select_bet
 
-    puts "bet #{bet} # with: #{my_cards} table: #{table_cards}"
-    bet
+    #puts "bet #{bet} # with: #{my_cards} table: #{table_cards}"
+    #bet
+    1000
   end
 
   def showdown(game_state)
