a4f4dc4cf26278f483a319abb1ab07bcd3fda136
Oleg V Artamonov <oleg@artamonov.ru>
2015-09-27 11:27:25 +0300

    :

---
+++ game_info.rb
@@ -0,0 +1,24 @@
+module GameInfo
+
+  def pair_in_hand?
+    my_cards[0] == my_cards[1]
+  end
+
+  def opponents
+    # 
+  end
+
+  def card
+
+  end
+
+  # returns 'AA' or '66' or '65' or '72'
+  def my_cards
+    me["hole_cards"].map {|card| card["rank"] }.join
+  end
+
+  def me
+    @game_state["players"].select{|p| p["name"] == "Mandarine"}[0]
+  end
+
+end
