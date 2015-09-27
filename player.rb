d7ef3a5863674137a1f90256d9141321fd8a3010
Oleg V Artamonov <oleg@artamonov.ru>
2015-09-27 10:53:45 +0300

    + some bet

--- player.rb
+++ player.rb
@@ -4,7 +4,8 @@ class Player
   VERSION = "Default Ruby folding player"
 
   def bet_request(game_state)
-    0
+    puts game_state["players"].inspect
+    rand(333)
   end
 
   def showdown(game_state)
